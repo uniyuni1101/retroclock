e2650d8739d0272768f4e07eec1e58a71df2f513
uniyuni1101 <uniyuni1101@gmail.com>
2023-07-05 22:06:18 +0900

    Add Ticker

--- engine/engine.go
+++ engine/engine.go
@@ -10,6 +10,55 @@ const (
 	DefaultDelayMS = 50 * time.Millisecond
 )
 
+type TickPerSecond int
+
+func (t TickPerSecond) Interval() time.Duration {
+	return time.Second / time.Duration(t)
+}
+
+type Ticker struct {
+	c             chan time.Time
+	tickPerSecond TickPerSecond
+	tickTime      time.Time
+	done          chan struct{}
+}
+
+func NewTicker(tick TickPerSecond) *Ticker {
+	ticker := &Ticker{
+		c:             make(chan time.Time),
+		tickPerSecond: tick,
+		tickTime:      time.Now(),
+	}
+	ticker.start()
+	return ticker
+}
+
+func (t *Ticker) start() {
+	go func() {
+	Loop:
+		for {
+			select {
+			case <-t.done:
+				break Loop
+			default:
+				time.Sleep(t.when())
+				t.tickTime = t.tickTime.Add(t.tickPerSecond.Interval())
+				t.c <- t.tickTime
+			}
+		}
+	}()
+}
+
+func (t *Ticker) when() time.Duration {
+	target := t.tickTime.Add(t.tickPerSecond.Interval())
+	return time.Until(target)
+}
+
+func (t *Ticker) Tick() time.Time {
+	tick := <-t.c
+	return tick
+}
+
 type Engine struct {
 	DelayMS time.Duration
 	Render  Renderer
--- engine/engine_test.go
+++ engine/engine_test.go
@@ -16,6 +16,36 @@ func (r *SpyRender) Render(t time.Time) {
 	r.callRender++
 }
 
+func TestTicker_CollectOfTickIntervals(t *testing.T) {
+	cases := map[string]struct {
+		tick          engine.TickPerSecond
+		count         int
+		totalDuration time.Duration
+	}{
+		"duration of 4 counts at 20 tick per second":  {20, 5, time.Second / 4},
+		"duration of 4 counts at 60 tick per second":  {40, 5, time.Second / 8},
+		"duration of 4 counts at 120 tick per second": {80, 5, time.Second / 16},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+
+			ticker := engine.NewTicker(c.tick)
+
+			t1 := time.Now()
+			times := []time.Time{}
+			for i := 0; i < c.count; i++ {
+				times = append(times, ticker.Tick())
+			}
+			t2 := time.Now()
+			got := t2.Sub(t1)
+
+			assertIntervalWIthinTolerance(t, times, c.tick.Interval(), c.tick.Interval() / 20)
+			assertDurationWithinTolerance(t, got, c.totalDuration, 5*time.Millisecond)
+		})
+	}
+}
+
 func TestEngine_RenderingPerTick(t *testing.T) {
 	cases := []struct {
 		tickTimes []time.Time
@@ -54,6 +84,26 @@ func TestEngine_RenderingPerTick(t *testing.T) {
 
 		if spyRender.callRender != c.want {
 			t.Errorf("call tick want: %d, got: %d", c.want, spyRender.callRender)
+		}
+	}
+}
+
+func assertDurationWithinTolerance(t *testing.T, got, want, tolerance time.Duration) {
+	t.Helper()
+
+	if got < want-tolerance || want+tolerance < got {
+		t.Errorf("got %s, want acceptable duration is %s±%s", got, want, tolerance)
+	}
+}
+
+func assertIntervalWIthinTolerance(t *testing.T, got []time.Time, delta, tolerance time.Duration) {
+	t.Helper()
+
+	for i, _ := range got[:len(got)-1] {
+		gotDelta := got[i+1].Sub(got[i]).Abs()
+
+		if gotDelta < delta-tolerance || delta+tolerance < gotDelta {
+			t.Errorf("got deltas %v, acceptable delta is %s±%s", got, delta, tolerance)
 		}
 	}
 }
