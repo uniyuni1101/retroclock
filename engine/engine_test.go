3adbaa3b68c3d96be76e76adcf158f397d49aab3
uniyuni1101 <uniyuni1101@gmail.com>
2023-07-05 10:39:51 +0900

    Fix Test Name and Test cases

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -9,23 +9,35 @@ import (
 
 type SpyRender struct {
 	callRender int
+	timeStack  []time.Time
 }
 
 func (r *SpyRender) Render(t time.Time) {
 	r.callRender++
 }
 
-func TestEngine(t *testing.T) {
-	spyRender := &SpyRender{}
-	engine := &engine.Engine{
-		DelayMS:  engine.DefaultDelayMS,
-		Renderer: spyRender,
+func TestEngine_RenderingPerTick(t *testing.T) {
+	cases := []struct {
+		tickTimes []time.Time
+		want      int
+	}{
+		{[]time.Time{time.Now(), time.Now(), time.Now()}, 3},
+		{[]time.Time{time.Now(), time.Now(), time.Now(), time.Now(), time.Now()}, 5},
 	}
 
-	want := 1
-	engine.Tick(time.Date(2023, 1, 1, 1, 23, 45, 0, time.UTC))
+	for _, c := range cases {
+		spyRender := &SpyRender{}
+		engine := &engine.Engine{
+			DelayMS: engine.DefaultDelayMS,
+			Render:  spyRender,
+		}
 
-	if spyRender.callRender != want {
-		t.Errorf("call tick want: %d, got: %d", want, spyRender.callRender)
+		for _, time := range c.tickTimes {
+			engine.Tick(time)
+		}
+
+		if spyRender.callRender != c.want {
+			t.Errorf("call tick want: %d, got: %d", c.want, spyRender.callRender)
+		}
 	}
 }
